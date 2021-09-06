00d9610a83f9460f2adb2f9699277f6a7774b13f
shivani22-18 <63472003+shivani22-18@users.noreply.github.com>
2021-09-07 00:45:02 +0530

    Add files via upload

---
+++ NodeJs/A1/node1-3.js
@@ -0,0 +1,14 @@
+var http = require('http');
+var fs = require('fs');
+
+path = require("path");
+
+var p = "./"
+fs.readdir(p,function(err,files){
+    if(err) throw err;
+
+    files.forEach(function(file){
+        if (fs.statSync(file).isDirectory()) console.log ("FOLDER: " + file);
+        else console.log("FILE: " + file);
+    })
+})
---
+++ NodeJs/A1/node1-7.js
@@ -0,0 +1,2 @@
+var strftime = require('strftime') // not required in browsers
+console.log(strftime('%B %d, %Y %H:%M:%S'));
